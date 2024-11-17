2c7ec49f1116ec33b0fceb8a9733dc885906ee10
Eliezer Nsengi <nsengitech@gmail.com>
2024-11-17 19:16:33 +0200

    add : nabbar ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./components/Landing";
+import Layout from "./components/ui/Layout";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Landing />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="bg-black text-white py-3 flex items-center justify-between px-10h">
+      <h2>zerSquare</h2>
+      <ul className="flex items-center justify between gap-2 py-2">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">Projects</Link>
+        </li>
+        <li>
+          <Link to="/">About Us</Link>
+        </li>
+        <li>
+          <Link to="/">Contact Us</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;

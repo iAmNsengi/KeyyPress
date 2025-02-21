a31dba410b8c625897864391d8a0eb3a3ef06480
iAmNsengi <iamnsengi@icloud.com>
2025-02-21 20:03:47 +0200

    ft: test if it works on pod

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,49 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import compression from "vite-plugin-compression";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression({
-      algorithm: "gzip",
-      ext: ".gz",
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
   build: {
+    minify: "terser",
     rollupOptions: {
       output: {
         manualChunks: {
           "react-vendor": ["react", "react-dom", "react-router-dom"],
-          "ui-components": ["@tabler/icons-react"],
         },
       },
     },
-    chunkSizeWarningLimit: 1000,
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom", "react-router-dom", "@tabler/icons-react"],
   },
   server: {
-    hmr: {
-      overlay: false,
-    },
+    port: 3000,
+    host: true,
   },
 });
